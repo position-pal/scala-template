13a0e8b15b28233aab29e838bc3b8513ace61faf
position-pal-mate <positionpal@gmail.com>
2025-03-06 16:34:12 +0000

    Deploying to gh-pages from @ position-pal/scala-template@d1eec3298cdce4a1baff72264070042784a13fad 🚀

--- scripts/searchData.js
+++ scripts/searchData.js
@@ -1,4 +1,4 @@
-pages = [{"l":"index.html#","e":false,"i":"","n":"scala-template 0.1.0-archeo+2cf294c API","t":"scala-template 0.1.0-archeo+2cf294c API","d":"","k":"static","x":""},
+pages = [{"l":"index.html#","e":false,"i":"","n":"scala-template 0.1.0-archeo+d1eec32 API","t":"scala-template 0.1.0-archeo+d1eec32 API","d":"","k":"static","x":""},
 {"l":"io/github/positionpal.html#","e":false,"i":"","n":"io.github.positionpal","t":"io.github.positionpal","d":"","k":"package","x":""},
 {"l":"io/github/positionpal/Launcher$.html#","e":false,"i":"","n":"Launcher","t":"Launcher","d":"io.github.positionpal","k":"object","x":""},
 {"l":"io/github/positionpal/Launcher$.html#helloWorld-94c","e":false,"i":"","n":"helloWorld","t":"helloWorld(): Unit","d":"io.github.positionpal.Launcher","k":"def","x":""},
